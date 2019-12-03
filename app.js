8ea9d9aadadb947c473bcc8afddf4afb483caa64
Tiziano Vicentini <tiziano@attividigitali.com>
2019-12-03 23:01:04 +0100

    api parameter

--- app.js
+++ app.js
@@ -7,11 +7,22 @@ var app = express();
 var PORT = process.env.PORT || 11235;
 
 app.get('/', function(req, res){
+
     res.send('<html><head></head><body><h2>Hello world!!</h2></body></html>');
+
 });
 
 app.get('/api', function(req, res){
+
     res.json( {"simon-says" : 'Hi guys!'} );
+
+});
+
+app.get('/api/number/:id', function(req, res){
+
+    res.send('<html><head></head><body><h2>Number inputted: ' +
+       '<code>' + req.params.id + '</code></h2>');
+
 });
 
 app.listen(PORT);
