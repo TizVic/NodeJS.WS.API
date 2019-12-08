be8f8570741f7ef325263b985b7943653d39f90c
Tiziano Vicentini <tiziano.vicentini@altromercato.it>
2019-12-08 15:17:05 +0100

    custom middleware example

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ var app = express();
 
 var PORT = process.env.PORT || 11235;
 
-// Middleware for ststic files
+// Middleware for static files
 app.use('/assets', express.static(__dirname + '/public'));
+
+// Minimal custom middleware [next()]
+app.use('/', function (req, res, next) {
+    console.log('Requested url: ' + req.url);
+    next();
+});
 
 app.get('/', function(req, res){
 
